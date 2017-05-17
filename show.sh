727667dfd044802060bd14841f373547c2176741
Bram Mittendorff <bram.mittendorff@dealerdirect.nl>
2017-05-17 17:35:06 +0200

    Added jq check

--- show.sh
+++ show.sh
@@ -1,4 +1,8 @@
-for f in wordpages/*.json
-do
-	echo "$(jq --arg file "$f" '.["entry_data"].TagPage[0].tag.media.count' "$f") ${f}"
-done
+if type jq > /dev/null 2>&1; then
+	for f in wordpages/*.json
+	do
+		echo "$(jq --arg file "$f" '.["entry_data"].TagPage[0].tag.media.count' "$f") ${f}"
+	done
+else
+	echo "Please install jq: https://stedolan.github.io/jq/"
+fi
