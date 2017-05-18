77fa54cb60df4e3712bdf81b70307de40d1f41d1
Bram Mittendorff <bram.mittendorff@dealerdirect.nl>
2017-05-18 10:34:14 +0200

    Added filtering out of filename

--- show.sh
+++ show.sh
@@ -1,7 +1,8 @@
 if type jq > /dev/null 2>&1; then
 	for f in wordpages/*.json
 	do
-		echo "$(jq --arg file "$f" '.["entry_data"].TagPage[0].tag.media.count' "$f") ${f}"
+		replaced_wordpages=${f/wordpages\//}
+		echo "$(jq --arg file "$f" '.["entry_data"].TagPage[0].tag.media.count' "$f") ${replaced_wordpages/.json/}"
 	done
 else
 	echo "Please install jq: https://stedolan.github.io/jq/"
