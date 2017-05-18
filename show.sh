31a438f31059a81d6adcd6e53a8f8af84446a7ad
Bram Mittendorff <bram.mittendorff@dealerdirect.nl>
2017-05-18 10:52:50 +0200

    Changed bad subsitution

--- show.sh
+++ show.sh
@@ -1,8 +1,7 @@
 if type jq > /dev/null 2>&1; then
 	for f in wordpages/*.json
 	do
-		replaced_wordpages=${f/wordpages\//}
-		echo "$(jq --arg file "$f" '.["entry_data"].TagPage[0].tag.media.count' "$f") ${replaced_wordpages/.json/}"
+		echo "$(jq --arg file "$f" '.["entry_data"].TagPage[0].tag.media.count' "$f") ${f}"
 	done
 else
 	echo "Please install jq: https://stedolan.github.io/jq/"
